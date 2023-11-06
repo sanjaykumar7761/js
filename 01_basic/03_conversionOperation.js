c4fe3b2884007b449c7edb4c8c63a021cf089955
sanjaykumar7761 <143536817+sanjaykumar7761@users.noreply.github.com>
2023-11-06 05:12:27 +0000

    conviersionOperation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+ let score =true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber )
+// console.log( valueInNumber )
+
+//"33"=> 33
+//"33abc"=> NaN
+//true => q; false=>0
+
+// let isLoggedIn =1 true
+//let isLoggedIn=""//false
+let isLoggedIn="sanjay"//true 
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true; 0=> false
+//""=> false
+//"hitesh"=> true
+
+let someNumber=33.4
+
+let StringNumber=String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
