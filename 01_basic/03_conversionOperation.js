864faa6dc6f181e4f203c1e7d88299eb244844cb
sanjaykumar7761 <143536817+sanjaykumar7761@users.noreply.github.com>
2023-11-06 05:50:05 +0000

    string conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -16,7 +16,7 @@
 let isLoggedIn="sanjay"//true 
 
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1=>true; 0=> false
 //""=> false
@@ -25,5 +25,34 @@ console.log(booleanIsLoggedIn)
 let someNumber=33.4
 
 let StringNumber=String(someNumber)
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+// ********************Operations**************
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="hello"
+let str2 =" Sanjay"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4)*5%3);
+
+console.log(+true)
+console.log(+"")
+
